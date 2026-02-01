50587403493e240909e9a17fd4aec9e868b3c88c
Jacob g <j.gonek01@gmail.com>
2026-02-01 11:40:45 -0700

    good

--- .github/Assignment1/Assignment1/Game1.cs
+++ .github/Assignment1/Assignment1/Game1.cs
@@ -8,7 +8,9 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-
+    private KeyboardState _kbPreviousState, _kbCurrentState;
+    private string _message = "";
+    private SpriteFont _font;
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -26,17 +28,60 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _font = Content.Load<SpriteFont>("SystemArialFont");
 
         // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        _kbCurrentState = Keyboard.GetState();
+        _message = "";
 
-        // TODO: Add your update logic here
+        #region arrow keys
+        
+        if(_kbCurrentState.IsKeyDown(Keys.Up))
+        {
+            _message += "Up ";
+        }
 
+        if(_kbCurrentState.IsKeyDown(Keys.Down))
+        {
+            _message += "Down ";
+        }
+
+        if(_kbCurrentState.IsKeyDown(Keys.Left))
+        {
+            _message += "Left ";
+        }
+
+        if(_kbCurrentState.IsKeyDown(Keys.Right))
+        {
+            _message += "Right ";
+        }
+
+        #endregion arrow keys
+        if(IsKeyHeld(Keys.Space))
+        {
+            _message += "\n";
+            _message += "Space pressed\n";
+            _message += "----------------------------------------\n";
+            _message += "----------------------------------------\n";
+            _message += "----------------------------------------\n";
+            _message += "----------------------------------------\n";
+            _message += "----------------------------------------\n";
+            _message += "----------------------------------------\n";
+            _message += "----------------------------------------\n";
+        }
+
+        else if(_kbPreviousState.IsKeyUp(Keys.Space))
+        {
+            _message += "\n";
+            _message += "Space not pressed";
+        }
+
+        _kbPreviousState = _kbCurrentState;
+        base.Update(gameTime);
         base.Update(gameTime);
     }
 
@@ -47,5 +92,9 @@ public class Game1 : Game
         // TODO: Add your drawing code here
 
         base.Draw(gameTime);
+    }
+    private bool IsKeyHeld(Keys key)
+    {
+        return _kbCurrentState.IsKeyDown(key);
     }
 }
